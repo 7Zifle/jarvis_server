fbc530465001dc511bd6dbde27fa1f2a8cb91254
Jacob Green <jacob.green863@gmail.com>
2023-02-20 20:21:19 -0500

    Basic tcp receiving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use zmq;
+
 fn main() {
-    println!("Hello, world!");
+    let context = zmq::Context::new();
+    let responder = context.socket(zmq::ROUTER).unwrap();
+
+    assert!(responder.bind("tcp://*:43599").is_ok());
+
+    println!("Starting loop");
+
+    loop {
+        let response = responder.recv_string(0).unwrap();
+        match response {
+            Ok(str) => println!("Response: {}", str),
+            Err(_e) => eprintln!("Error: "),
+        }
+    }
 }
