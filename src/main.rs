1a51e5b00ff5edea5f00485090f4d8f0b5b1fa73
Jacob Green <jacob.green863@gmail.com>
2023-02-21 20:22:45 -0500

    Updated time based sending

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use chrono::prelude::*;
 use zmq;
-
 fn main() {
+    println!("Starting jarvis");
     let context = zmq::Context::new();
     let responder = context.socket(zmq::ROUTER).unwrap();
 
@@ -9,10 +10,22 @@ fn main() {
     println!("Starting loop");
 
     loop {
-        let response = responder.recv_string(0).unwrap();
-        match response {
-            Ok(str) => println!("Response: {}", str),
-            Err(_e) => eprintln!("Error: "),
+        let msg = responder.recv_multipart(0).expect("Unable to view message");
+        println!("received msg");
+        let id = format!("ID: {:X?}", msg[0].clone());
+        println!("id: {}", id);
+        // we will ignore msg[1] since its padding
+        let msg = String::from_utf8(msg[2].clone()).expect("Unable to convert msg[2] to string");
+        let current_time: DateTime<Local> = Local::now();
+        if current_time.hour() >= 6 && current_time.hour() < 22 {
+            // start playing music
         }
+        println!("msg: {}", msg);
     }
+}
+#[cfg(test)]
+mod tests {
+    use chrono::prelude::*;
+    #[test]
+    fn should_be_playing() {}
 }
